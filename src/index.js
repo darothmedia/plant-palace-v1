b29f8cd5ee3f048dd67639ffc47c1e3d15304538
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-29 14:04:27 -0700

    adjust modules

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ function create() {
   const logo = this.physics.add.image(400, 100, 'logo')
 
   logo.setVelocity(100, 200)
-  logo.setBounce(1, 1)
-  logo.setCollideWorldBounds(true)
+  logo.setBounce(1, 2)
+  logo.setCollideWorldBounds(false)
 
-  emitter.startFollow(logo)
+  // emitter.startFollow(logo)
 }
 
 const config = {
